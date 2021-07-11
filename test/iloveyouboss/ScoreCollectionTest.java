3837caf75bc4daf66b5223317f37e03ef9301e9a
bpkim <kbp0213@gmail.com>
2021-07-12 00:09:23 +0900

    Create answersArtithmeticMeanOfTwoNumbers Method

--- test/iloveyouboss/ScoreCollectionTest.java
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -1,6 +1,7 @@
 package iloveyouboss;
 
 // fail 메소드가 있다.
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 // Test 애너테이션이 있다.
 import org.junit.*;
@@ -16,5 +17,19 @@ public class ScoreCollectionTest {
     public void test() {
         // 실패를 일으키는 메소드
 //        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void answersArtithmeticMeanOfTwoNumbers(){
+        // 준비 
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+        
+        // 실행
+        int actualResult = collection.arithmeticMean();
+        
+        // 단언
+        assertThat(actualResult, equalTo(6));
     }
 }
